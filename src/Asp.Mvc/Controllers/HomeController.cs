2b3e1b93e318f2b0962a45eab9029a8330ccaedc
Yury Shapkin <yshapkin@hotmail.com>
2015-10-27 00:14:13 +0300

    +

--- src/Asp.Mvc/Controllers/HomeController.cs
+++ src/Asp.Mvc/Controllers/HomeController.cs
@@ -23,5 +23,24 @@ namespace Asp.Mvc.Controllers
             var path = Path.Combine(dir, id + ".jpg");
             return File(path, "image/jpeg");
         }
+
+        [OutputCache(Duration = 3600, VaryByParam = "id")]
+        public ActionResult GetOutputCache(int id)
+        {
+            var dir = Server.MapPath("/Images");
+            var path = Path.Combine(dir, id + ".jpg");
+            return File(path, "image/jpeg");
+        }
+
+        public ActionResult RemoveCache(int id)
+        {
+            var url = Url.Action(
+                "GetOutputCache",
+                "Home",
+                new {id = id}
+                );
+            HttpResponse.RemoveOutputCacheItem(url);
+            return RedirectToAction("GetOutputCache", new { id = id });
+        }
     }
 }
